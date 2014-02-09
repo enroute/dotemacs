a2841aac364d757138ff7a0523849364a214eb0e
enroute <enroute.smth@gmail.com>
2014-02-09 22:01:47 +0800

    add eim

--- .emacs.d/config/03key.el
+++ .emacs.d/config/03key.el
@@ -1,8 +1,5 @@
-(global-set-key "\M-w" 'clipboard-kill-ring-save)
-(global-set-key "\C-xk" 'kill-this-buffer)
-
-;;; make \C-k, <deleteline> kill a whole line
-(setq kill-whole-line t)
+(global-set-key "\M-w" 'clipboard-kill-ring-save) ; make M-w also save to clipboard
+(global-set-key "\C-xk" 'kill-this-buffer)	  ; kill current buffer rather than asking
 
 
 (provide '03key)
