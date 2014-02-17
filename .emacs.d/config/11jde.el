e21639ffd0d5973fc7ff065a1a8cfcd24ebd3aad
boastain <boastain@gmail.com>
2014-02-17 22:22:54 +0800

    change jdee-2.4.1, whose `jde-wiz-implement-interface\' not working well, back to jdee2.4.0.1

--- .emacs.d/config/11jde.el
+++ .emacs.d/config/11jde.el
@@ -1,8 +1,28 @@
 ;;; jdee, for java
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/jdee/lisp"))
+
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/cedet/cogre"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/cedet/common"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/cedet/contrib"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/cedet/ede"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/cedet/eieio"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/cedet/semantic"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/cedet/speedbar"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/cedet/srecode"))
+
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/elib"))
+
 (require 'cedet)
 (require 'jde)
+
+(setq semantic-load-turn-useful-things-on t)
+
+;; Disable version check. Current version of jdee supports only lower
+;; version of cedet
+(setq jde-check-version-flag nil)
+
+;; for obsolete functions in emacs 24
+(defun screen-width nil -1)
+(define-obsolete-function-alias 'make-local-hook 'ignore "21.1")
 
 (provide '11jde)
