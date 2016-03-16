d21d9d7a25cb8c8b37076e5c8c05bde28ec01b51
Mario Li <mario.li@tcl.com>
2016-03-16 09:03:09 +0800

    Ignore all hidden files in dired-omit-mode

--- .emacs.d/config/05dired.el
+++ .emacs.d/config/05dired.el
@@ -8,6 +8,9 @@
      
 ;; dired-mode-map key-bindings
 (add-hook 'dired-mode-hook (lambda ()
+  ;; Omit hidden files
+  (setq dired-omit-files (concat dired-omit-files "\\|^\\..*$"))
+  (dired-omit-mode t)
   ;;; W -> use emacs-w3m to browse the file of current line
   (when (featurep 'w3m)
     (define-key dired-mode-map (kbd "W") 'my-browse-with-emacs-w3m))))
@@ -17,3 +20,20 @@
   (let ((file (dired-get-file-for-visit)))
     (when (featurep 'w3m)
       (w3m-browse-url file))))
+
+;; Toggle showing dot files.
+;; Use `dired-omit-files' and `dired-omit-extensions' in `dired-omit-mode' instead.
+;; (defun dired-dotfiles-toggle ()
+;;   "Show/hide dot-files"
+;;   (interactive)
+;;   (when (equal major-mode 'dired-mode)
+;;     (if (or (not (boundp 'dired-dotfiles-show-p)) dired-dotfiles-show-p) ; if currently showing
+;;         (progn 
+;;           (set (make-local-variable 'dired-dotfiles-show-p) nil)
+;;           (message "h")
+;;           (dired-mark-files-regexp "^\\\.")
+;;           (dired-do-kill-lines))
+;;       (progn (revert-buffer) ; otherwise just revert to re-show
+;;              (set (make-local-variable 'dired-dotfiles-show-p) t)))))
+
+;; 
