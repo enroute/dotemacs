6579fa554d06d7fc1d4a90069cef92f1708b57c1
Mario Li <mario.li@tcl.com>
2016-04-20 09:25:28 +0800

    Small fix

--- .emacs.d/config/00func.el
+++ .emacs.d/config/00func.el
@@ -16,18 +16,20 @@
           (tmp   0)
           (rows 32))
       (insert (propertize
-               "                     [ASCII table]\n\n"
+               "                                           [ASCII table]\n\n"
                'face font-lock-comment-face))
       (insert (propertize
-               (concat "DEC [0X]  SYM | DEC [0X]  SYM | DEC [0X]  SYM | DEC [0X]  SYM\n"
-                       "--------------+---------------+---------------+--------------\n")
+               (concat "DEC [0X] [BINARY]  SYM | DEC [0X] [BINARY]  SYM | DEC [0X] [BINARY]  SYM | DEC [0X] [BINARY]  SYM\n"
+                       "-----------------------+------------------------+------------------------+-----------------------\n")
                'face font-lock-comment-face))
       (while (< i rows)
         (dolist (tmp (list i (+ rows i) (+ rows rows i) (+ rows rows rows i)))
           (insert (concat
                    (propertize (format "%3d " tmp)
                                'face font-lock-function-name-face)
-                   (propertize (format "[%2x]" tmp)
+                   (propertize (format "[%2x] " tmp)
+                               'face font-lock-constant-face)
+                   (propertize (my-number-to-binary-string tmp 8)
                                'face font-lock-constant-face)
                    "  "
                    (propertize (format "%3s" (single-key-description tmp))
@@ -74,7 +76,7 @@ COMMAND."
       (setq word (read-string "Enter word to translate: ")))
     (shell-command (concat "sdcv" " " word))))
 
-(defun my-number-to-binary-string (i)
+(defun my-number-to-binary-string (i &optional len)
   "convert an integer into it's binary representation in string format"
   (let ((res ""))
     (while (not (= i 0))
@@ -82,4 +84,6 @@ COMMAND."
       (setq i (lsh i -1)))
     (if (string= res "")
         (setq res "0"))
+    (if (and (numberp len) (> len (length res)))
+        (setq res (concat (make-string (- len (length res)) ?0) res)))
     res))
