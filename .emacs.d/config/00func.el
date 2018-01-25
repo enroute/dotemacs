aa78a02fb1f83c6a3434714a60eacaa8196acdd7
Mario Li <mario.li@tcl.com>
2018-01-25 09:57:20 +0800

    Check ffap string for line-number and goto it

--- .emacs.d/config/00func.el
+++ .emacs.d/config/00func.el
@@ -87,3 +87,36 @@ COMMAND."
     (if (and (numberp len) (> len (length res)))
         (setq res (concat (make-string (- len (length res)) ?0) res)))
     res))
+
+
+;; https://www.emacswiki.org/emacs/FindFileAtPoint
+;; Check ffap string for line-number and goto it (using advice)
+;; Useful e.g. for output from grep -nH. Expects file-name:line-number.
+(defvar ffap-file-at-point-line-number nil
+  "Variable to hold line number from the last `ffap-file-at-point' call.")
+
+(defadvice ffap-file-at-point (after ffap-store-line-number activate)
+    "Search `ffap-string-at-point' for a line number pattern and
+save it in `ffap-file-at-point-line-number' variable."
+    (let* ((string (ffap-string-at-point)) ;; string/name definition copied from `ffap-string-at-point'
+           (name
+            (or (condition-case nil
+                    (and (not (string-match "//" string)) ; foo.com://bar
+                         (substitute-in-file-name string))
+                  (error nil))
+                string))
+           (line-number-string
+            (and (string-match ":[0-9]+" name)
+                 (substring name (1+ (match-beginning 0)) (match-end 0))))
+           (line-number
+            (and line-number-string
+                 (string-to-number line-number-string))))
+      (if (and line-number (> line-number 0))
+          (setq ffap-file-at-point-line-number line-number)
+        (setq ffap-file-at-point-line-number nil))))
+
+(defadvice find-file-at-point (after ffap-goto-line-number activate)
+  "If `ffap-file-at-point-line-number' is non-nil goto this line."
+  (when ffap-file-at-point-line-number
+    (goto-line ffap-file-at-point-line-number)
+        (setq ffap-file-at-point-line-number nil)))
